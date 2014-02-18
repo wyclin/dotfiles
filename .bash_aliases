67b51acf3c4e8a26e7434b2417895645394cac81
Wayne Lin <wayne@wyclin.com>
2014-02-18 18:16:53 -0500

    Add aliases in .bash_aliases

---
+++ .bash_aliases
@@ -0,0 +1,1 @@
+alias rails-precompile="rm -rf public/assets; RAILS_ENV=production rake assets:precompile"
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim .gemrc .zshenv .rspec"     # list of files/folders to symlink in homedir
+files=".bashrc .bash_aliases .vimrc .vim .gemrc .zshenv .rspec"     # list of files/folders to symlink in homedir
 
 ##########
 
